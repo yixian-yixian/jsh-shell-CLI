4012290d59e4dfb42fb2a1fd2996eb2794bb5c5f
Ivian Zhang <yzhang41@couchvm01.eecs.tufts.edu>
2023-06-10 00:30:30 -0400

    implemented simple commandline handling to process user input

--- shell.c
+++ shell.c
@@ -28,8 +28,7 @@ void execute_cmd(char *cmd[]) {
  */
 void create_pipes(int pipefds[], int num_pipes) {
     for (int i = 0; i < num_pipes; i++) {
-        if (pipe(pipefds + i*2) < 0) { // TODO: check if this is correct
-            perror("jsh: Unsuccesful pipe creation\n");
+        if (pipe(pipefds + i*2) < 0) { // TODO: check expected error output
             exit(CHILD_PROCESS);
         }
     }
@@ -53,7 +52,7 @@ void redirect_execution(char *cmd[], int pipefds[], int i, int num_pipes) {
     if (i != 0) {
         if (dup2(pipefds[(i-1)*2], 0) < 0) {
             perror("dup2"); // stdin
-            exit(EXIT_FAILURE);
+            exit(CHILD_PROCESS);
         }
     }
 
@@ -61,7 +60,7 @@ void redirect_execution(char *cmd[], int pipefds[], int i, int num_pipes) {
     if (i != num_pipes - 1) {
         if (dup2(pipefds[i*2 + 1], 1) < 0) {
             perror("dup2"); // stdout
-            exit(EXIT_FAILURE);
+            exit(CHILD_PROCESS);
         }
     }
     close_all_pipes(pipefds, num_pipes);
@@ -70,19 +69,17 @@ void redirect_execution(char *cmd[], int pipefds[], int i, int num_pipes) {
 }
 
 /* launch_child_processes 
- * purpose: 
- * notes: 
+ * purpose: launch child processes that will execute commands
+ * notes: exit status 127 if child process creation fails
  */
 void launch_child_process(char *cmds[][MAX_CMD_LENGTH], int pipefds[], int num_pipes) {
     int pid = 0;
     for (int i = 0; i < num_pipes; i++) {
         // create child process
         pid = fork();
-        if (pid == 0) {
-            // launch child process
+        if (pid == 0) { // child process created
             redirect_execution(cmds[i], pipefds, i, num_pipes);
         } else if (pid < 0) {
-            fprintf(stderr, "jsh: Error creating child process\n");
             exit(CHILD_PROCESS);
         }
     }
@@ -131,11 +128,11 @@ int main() {
     while (fgets(user_input, MAX_CMD_LENGTH, stdin) != NULL) {
         // remove newline from user input
         user_input[strcspn(user_input, "\n")] = 0;
-        if (strcmp(user_input, "exit") == 0) {
+        if (strcmp(user_input, "exit") == 0) 
             exit(0);
-        } else {
+        else 
             process_input(user_input);
-        }
+    
         fprintf(stdout, "jsh$ ");
     }
     return 0;
